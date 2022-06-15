b63371252583954bf65c06a80178b86753f32aad
Елена Малолепская <03054177@mail.ru>
2022-06-15 18:28:12 +0300

    Gud body

--- src/test/java/ru/netology/postman/PostmanEchoTest.java
+++ src/test/java/ru/netology/postman/PostmanEchoTest.java
@@ -16,6 +16,6 @@ public class PostmanEchoTest {
                 .post("/post")
                 .then()
                 .statusCode(200)
-                .body("data", equalTo("Домашка по математике."));
+                .body("data", equalTo("Домашка по Постману."));
     }
 }
